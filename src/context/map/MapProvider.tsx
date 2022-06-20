f93144bda0e9ad7b82301b3dc0ef9f6cfaae0761
jantonio2 <jesusrochavedia@gmail.com>
2022-06-19 21:09:53 -0400

    Creando contexto para el mapa

---
+++ src/context/map/MapProvider.tsx
@@ -0,0 +1,31 @@
+import { useReducer } from 'react';
+import { MapContext } from './MapContext';
+import { Map } from 'mapbox-gl';
+import { mapRedducer } from './mapReducer';
+
+export interface MapState {
+  isMapReady: boolean;
+  map?: Map;
+}
+
+const INITIAL_STATE: MapState = {
+  isMapReady: false,
+  map: undefined,
+}
+
+interface Props {
+  children: JSX.Element | JSX.Element[];
+}
+
+export const MapProvider = ({ children }: Props) => {
+  
+  const [state, dispatch] = useReducer(mapRedducer, INITIAL_STATE);
+
+  return (
+    <MapContext.Provider value={{
+      ...state
+    }}>
+      { children }
+    </MapContext.Provider>
+  );
+}
